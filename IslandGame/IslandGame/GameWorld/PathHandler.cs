26b11fcd00ebef478f2c525fec2ea1e84f9c6e56
ijdykeman <ijdykeman@gmail.com>
2014-05-22 21:26:01 -0400

    Return to old pathing algo
    With the pathingProfile fix, the old algo is ideal.

--- IslandGame/IslandGame/GameWorld/PathHandler.cs
+++ IslandGame/IslandGame/GameWorld/PathHandler.cs
@@ -33,7 +33,9 @@ namespace IslandGame.GameWorld
 
         }
 
-        protected virtual List<BlockLoc> PathOLD(IslandPathingProfile startProfile, ref BlockLoc startLoc, HashSet<BlockLoc> goals, int heightOfEntity)
+
+
+        protected virtual List<BlockLoc> Path(IslandPathingProfile startProfile, ref BlockLoc startLoc, HashSet<BlockLoc> goals, int heightOfEntity)
         {
             foreach (BlockLoc goal in goals)
             {
@@ -51,83 +53,47 @@ namespace IslandGame.GameWorld
             PathNodePriorityQueue openNodes = new PathNodePriorityQueue();
             HashSet<BlockLoc> visitedLocations = new HashSet<BlockLoc>();
 
-
             openNodes.insertNode(new PathNode(null, startLoc, 0, goals.First()));
 
             IslandPathingProfile profile = startProfile;
             while (openNodes.size() > 0)
             {
-                //return null;
                 PathNode from = openNodes.pop();
 
                 List<BlockLoc> nextSteps = profile.getSpacesThatCanBeMovedToFrom(from.loc, heightOfEntity);
 
-                //adding new nodes to the openNodes array
-
+                //adding new nodes to the openNodes unmippedArray
                 foreach (BlockLoc next in nextSteps)
                 {
-                    if (visitedLocations.Contains(next))
+                    if (!visitedLocations.Contains(next))
                     {
 
-                        continue;
-                    }
+                        PathNode toAdd = new PathNode(from, next, from.costToGetHere + 1, goals.First());
 
-                    PathNode toAdd = new PathNode(from, next, from.costToGetHere + 1, goals.First());
-
-                    if (goals.Contains(toAdd.loc))
-                    {
-                        List<BlockLoc> finalPath = getPathListFromEnd(toAdd);
-                        finalPath.RemoveAt(0);
-                        finalPath.Add(toAdd.loc);
+                        if (goals.Contains(toAdd.loc))
+                        {
+                            List<BlockLoc> finalPath = getPathListFromEnd(toAdd);
+                            finalPath.RemoveAt(0);
+                            finalPath.Add(toAdd.loc);
 
 
-                        Console.WriteLine(finalPath.Count);
-                        return finalPath;
-                    }
+                            Console.WriteLine(finalPath.Count);
+                            return finalPath;
+                        }
 
-                    bool skipThis = false;
-                    bool neighborIsBetter = false;
-                    PathNode alreadyInOpenNodes = openNodes.getNodeWithLoc(toAdd.loc);
-                    if (alreadyInOpenNodes == null)
-                    {
-                        //if (alreadyInOpenNodes.costToGetHere < toAdd.costToGetHere)
-                        // {
-                        skipThis = true;
-                        neighborIsBetter = true;
-                        //}
-                    }
-
-                   /* else if (visitedLocations.Contains(toAdd.loc))
-                    {
-
-                        skipThis = true;
-                        neighborIsBetter = true;
-                    }*/
-                    else
-                    {
-                        neighborIsBetter = false;
-                    }
-
-                    if (neighborIsBetter)
-                    {
                         openNodes.insertNode(toAdd);
+                        visitedLocations.Add(next);
+
                     }
-
-
-
-
-
                 }
-                visitedLocations.Add(from.loc);
 
-                //return null;
+
 
             }
             return null;//no path found
         }
 
-
-        protected virtual List<BlockLoc> Path(IslandPathingProfile startProfile, ref BlockLoc startLoc, HashSet<BlockLoc> goals, int heightOfEntity)
+        protected virtual List<BlockLoc> PathNEW(IslandPathingProfile startProfile, ref BlockLoc startLoc, HashSet<BlockLoc> goals, int heightOfEntity)
         {
             foreach (BlockLoc goal in goals)
             {
@@ -181,7 +147,7 @@ namespace IslandGame.GameWorld
 
                     if (openNodes.hasNodeWithLoc(next))
                     {
-                        continue;
+                         continue;
                     }
 
 
